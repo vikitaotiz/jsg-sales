b8feef8a67d81599ca4a4e2e4de0382251b71873
vikitaotiz <victorotieno598@gmail.com>
2023-07-21 06:54:30 +0300

    first commit

---
+++ assets/helpers.58d4ed16.js
@@ -0,0 +1,1 @@
+const a="http://jsg.missnadhifu.com/laravel/public/api/v1";export{a as b};
---
+++ assets/vuequery.3b7d2f97.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.e11e7adc.js";import"./index.36f77fb2.js";var o=({app:e})=>{e.use(r)};export{o as default};

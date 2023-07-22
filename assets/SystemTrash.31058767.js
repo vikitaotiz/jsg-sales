3a07f77fe8e74eb855013d20e11e3d7e534a9cc2
vikitaotiz <victorotieno598@gmail.com>
2023-07-22 17:37:56 +0300

    sales4

---
+++ assets/SystemTrash.31058767.js
@@ -0,0 +1,1 @@
+import{c4 as e,aI as r,a5 as a}from"./index.bc27483f.js";const c={};function s(t,n){return r(),a("div",null,"Trash")}var _=e(c,[["render",s]]);export{_ as default};
---
+++ assets/vuequery.b335c0c9.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.4c42915d.js";import"./index.bc27483f.js";var o=({app:e})=>{e.use(r)};export{o as default};

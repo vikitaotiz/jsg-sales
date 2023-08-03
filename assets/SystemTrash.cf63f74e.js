25b3aadd0920f389fa865c3db1c4c30b4428e43e
vikitaotiz <victorotieno598@gmail.com>
2023-08-03 22:59:35 +0300

    Debts

---
+++ assets/SystemTrash.cf63f74e.js
@@ -0,0 +1,1 @@
+import{c4 as e,aI as s,a5 as a}from"./index.e2e1918c.js";const c={},o={class:"q-pa-md"};function r(t,n){return s(),a("div",o,"Coming soon...")}var m=e(c,[["render",r]]);export{m as default};
---
+++ assets/vuequery.440e30a2.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.db2df67e.js";import"./index.e2e1918c.js";var o=({app:e})=>{e.use(r)};export{o as default};

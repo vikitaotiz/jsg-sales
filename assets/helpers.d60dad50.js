997bd066685d751c1f67703b7e46e91dcd7a65d6
vikitaotiz <victorotieno598@gmail.com>
2023-08-05 09:45:33 +0300

    sales17

---
+++ assets/helpers.d60dad50.js
@@ -0,0 +1,1 @@
+const a="https://jsg.missnadhifu.com/laravel/public/api/v1",e=o=>{o===500&&(localStorage.removeItem("auth_user"),window.location.reload(!0))};export{a as b,e as f};
---
+++ assets/vuequery.3e10827a.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.ec001523.js";import"./index.f65f6c89.js";var o=({app:e})=>{e.use(r)};export{o as default};

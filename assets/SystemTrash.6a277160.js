df013a6c16ca492f4af6246f3ea52169234e04e5
vikitaotiz <victorotieno598@gmail.com>
2023-08-07 06:48:05 +0300

    sales19

---
+++ assets/SystemTrash.6a277160.js
@@ -0,0 +1,1 @@
+import{c8 as e,aJ as s,a6 as a}from"./index.bda02627.js";const c={},o={class:"q-pa-md"};function r(t,n){return s(),a("div",o,"Coming soon...")}var m=e(c,[["render",r]]);export{m as default};
---
+++ assets/vuequery.d26265d7.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.fe2427df.js";import"./index.bda02627.js";var o=({app:e})=>{e.use(r)};export{o as default};

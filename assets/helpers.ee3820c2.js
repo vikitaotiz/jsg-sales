12200458611f2ab7d10a0e3826c6d5265a947cb7
vikitaotiz <victorotieno598@gmail.com>
2023-08-07 06:56:47 +0300

    sales20

---
+++ assets/helpers.ee3820c2.js
@@ -0,0 +1,6 @@
+const a="https://jsg.missnadhifu.com/laravel/public/api/v1",s=e=>{e===500&&(localStorage.removeItem("auth_user"),window.location.reload(!0))},i=`
+  <div style="text-align: center;">
+  <b>Jam Street Grocery</b>
+  <p>Nairobi, Eastleigh Area</p>
+  </div>
+`,r=e=>!!(e.length>0&&(e.some(t=>t.name==="Admin")||e.some(t=>t.name==="Cashier")));export{i as a,a as b,s as f,r as h};
---
+++ assets/vuequery.bd6eacdf.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.5dbee558.js";import"./index.48ee8118.js";var o=({app:e})=>{e.use(r)};export{o as default};
